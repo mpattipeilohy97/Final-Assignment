63c7b24e5c651a34bbd5e497bc2866e784e6b019
mpattipeilohy97 <marit.pattipeilohy@ru.nl>
2024-03-23 15:54:38 +0100

    final version I hope

--- bam2bigwig.sh
+++ bam2bigwig.sh
@@ -5,6 +5,7 @@ input_dir=$1
 output_dir=$2
 
 mkdir -p "$output_dir"
+mkdir -p "$output_dir/temporary"
 
 if [ ! -d "$input_dir" ]; then
 	echo "'input_dir' not found."
@@ -16,8 +17,9 @@ source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
 conda create -n "bam2bigwig" deeptools samtools --yes &>> $log_file
 conda activate bam2bigwig
 
+cp -r "$input_dir"/* "$output_dir/temporary"
 
-for bam_file in "$input_dir"/*.bam; do
+for bam_file in "$output_dir/temporary"/*.bam; do
     if [ -e "$bam_file" ]; then
         # Index the BAM file
 
@@ -25,12 +27,14 @@ for bam_file in "$input_dir"/*.bam; do
     fi
 done
 
-for indexed_bam_file in "$input_dir"/*.bam; do
+for indexed_bam_file in "$output_dir/temporary"/*.bam; do
     if [ -e "${indexed_bam_file}.bai" ]; then
         file_name=$(basename "$indexed_bam_file" .bam)
 
-        nice bamCoverage --bam "$indexed_bam_file" -o "$output_dir/$file_name.bw" &>> "$log_file"
+        nice bamCoverage --bam "$indexed_bam_file" -o "$output_dir/$file_name.bw" &>> $log_file
     fi
 done
+
+rm -r "$output_dir/temporary"
 
 echo "mpattipeilohy"
