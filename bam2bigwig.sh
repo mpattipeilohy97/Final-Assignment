ee699ef701c53713c61460fd9d4d8cd3ec77208e
mpattipeilohy97 <marit.pattipeilohy@ru.nl>
2024-03-23 11:43:50 +0100

    updated script

--- bam2bigwig.sh
+++ bam2bigwig.sh
@@ -13,7 +13,7 @@ fi
 log_file="$output_dir/log.txt"
 
 source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
-conda create -n "bam2bigwig" deeptools samtools --yes  &>> $log_file
+conda create -n "bam2bigwig" deeptools samtools --yes &>> $log_file
 conda activate bam2bigwig
 
 
@@ -29,9 +29,8 @@ for indexed_bam_file in "$input_dir"/*.bam; do
     if [ -e "${indexed_bam_file}.bai" ]; then
         file_name=$(basename "$indexed_bam_file" .bam)
 
-        nice bamCoverage --bam "$indexed_bam_file" -o "$output_dir/$file_name.bw" &>> $log_file
+        nice bamCoverage --bam "$indexed_bam_file" -o "$output_dir/$file_name.bw" &>> "$log_file"
     fi
 done
 
-
-echo "Marit Pattipeilohy"
+echo "mpattipeilohy"
